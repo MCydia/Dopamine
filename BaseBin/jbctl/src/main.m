86f32b361147595739f3da97a0e3d197ab09f217
MCydia <61176750+MCydia@users.noreply.github.com>
2023-06-16 21:25:04 +0800

    Update main.m

--- BaseBin/jbctl/src/main.m
+++ BaseBin/jbctl/src/main.m
@@ -11,7 +11,9 @@ void print_usage(void)
 Available commands:\n\
 	proc_set_debugged <pid>\t\tMarks the process with the given pid as being debugged, allowing invalid code pages inside of it\n\
 	rebuild_trustcache\t\tRebuilds the TrustCache, clearing any previously trustcached files that no longer exists from it (automatically ran daily at midnight)\n\
-	update <tipa/basebin> <path>\tInitiates a jailbreak update either based on a TIPA or based on a basebin.tar file, TIPA installation depends on TrollStore, afterwards it triggers a userspace reboot\n");
+	update <tipa/basebin> <path>\tInitiates a jailbreak update either based on a TIPA or based on a basebin.tar file, TIPA installation depends on TrollStore, afterwards it triggers a userspace reboot\n\
+	bindmount_path <source_path>\tFor a valid given source <source_path>, copy its contents and mount it onto `/var/jb/<source_path>`. This could be used to modify system files.\n\
+	bindunmount_path <source_path>\tThis reverts what `bindmount_path` does.\n");
 }
 
 int main(int argc, char* argv[])
@@ -57,10 +59,29 @@ int main(int argc, char* argv[])
 			printf("Update applied, userspace rebooting to finalize it...\n");
 			sleep(2);
 			return reboot3(RB2_USERREBOOT);
+			// execve(prebootPath(@"usr/bin/launchctl").fileSystemRepresentation, (char *const[]){ (char *const)prebootPath(@"usr/bin/launchctl").fileSystemRepresentation, "reboot", "userspace", NULL }, environ);
+			// return 0;
 		}
 		else {
 			printf("Update failed with error code %lld\n", result);
 			return result;
+		}
+	} else if (!strcmp(cmd, "userspace_reboot")) {
+		execve(prebootPath(@"usr/bin/launchctl").fileSystemRepresentation,
+			(char *const[]){
+				(char *const)prebootPath(@"usr/bin/launchctl").fileSystemRepresentation, "reboot", "userspace", NULL
+			}, environ);
+	} else if (!strcmp(cmd, "bindmount_path")) {
+		if (argc != 3) {
+			return 1;
+		} else {
+			jbdBindMountPath([NSString stringWithUTF8String:argv[2]], true);
+		}
+	} else if (!strcmp(cmd, "bindunmount_path")) {
+		if (argc != 3) {
+			return 1;
+		} else {
+			jbdBindUnmountPath([NSString stringWithUTF8String:argv[2]]);
 		}
 	}
 

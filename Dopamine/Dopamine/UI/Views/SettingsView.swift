823b11f8e446e8c3046de14513732e820ca40924
MCydia <61176750+MCydia@users.noreply.github.com>
2023-06-16 21:35:08 +0800

    Update SettingsView.swift

--- Dopamine/Dopamine/UI/Views/SettingsView.swift
+++ Dopamine/Dopamine/UI/Views/SettingsView.swift
@@ -9,38 +9,51 @@ import SwiftUI
 import Fugu15KernelExploit
 
 struct SettingsView: View {
-    
+
     @AppStorage("totalJailbreaks", store: dopamineDefaults()) var totalJailbreaks: Int = 0
     @AppStorage("successfulJailbreaks", store: dopamineDefaults()) var successfulJailbreaks: Int = 0
-    
+
     @AppStorage("verboseLogsEnabled", store: dopamineDefaults()) var verboseLogs: Bool = false
     @AppStorage("tweakInjectionEnabled", store: dopamineDefaults()) var tweakInjection: Bool = true
     @AppStorage("iDownloadEnabled", store: dopamineDefaults()) var enableiDownload: Bool = false
-    
+
     @Binding var isPresented: Bool
-    
+
+    @AppStorage("rebuildEnvironment", store: dopamineDefaults()) var rebuildEnvironment: Bool = false
+    @AppStorage("pathMappingEnabled", store: dopamineDefaults()) var pathMappingEnabled: Bool = true
+    @AppStorage("blockDopamineUpdates", store: dopamineDefaults()) var blockDopamineUpdates: Bool = false
+    @State var pathMappingAlertShown = false
+    @State var pathMappingInput = ""
+    @State var pathMappingRemoveAlertShown = false
+    @State var pathMappingRemoveInput = ""
+
     @State var mobilePasswordChangeAlertShown = false
     @State var mobilePasswordInput = "alpine"
-    
+
     @State var removeJailbreakAlertShown = false
     @State var isSelectingPackageManagers = false
     @State var tweakInjectionToggledAlertShown = false
-    
+
     @State var isEnvironmentHiddenState = isEnvironmentHidden()
-    
+
     @State var easterEgg = false
-    
+
     init(isPresented: Binding<Bool>?) {
         UIView.appearance(whenContainedInInstancesOf: [UIAlertController.self]).tintColor = .init(named: "AccentColor")
         self._isPresented = isPresented ?? .constant(true)
     }
-    
+
     var body: some View {
         VStack {
             if !isSelectingPackageManagers {
                 VStack {
                     VStack(spacing: 20) {
                         VStack(spacing: 10) {
+                            if !isJailbroken() {
+                                Toggle("Options_Rebuild_Environment", isOn: $rebuildEnvironment)
+                                Toggle("Options_Enable_Path_Mapping", isOn: $pathMappingEnabled)
+                            }
+                            Toggle("Options_Block_Dopamine_Updates", isOn: $blockDopamineUpdates)
                             Toggle("Settings_Tweak_Injection", isOn: $tweakInjection)
                                 .onChange(of: tweakInjection) { newValue in
                                     if isJailbroken() {
@@ -61,6 +74,44 @@ struct SettingsView: View {
                         if isBootstrapped() {
                             VStack {
                                 if isJailbroken() {
+                                    if (isPathMappingEnabled()) {
+                                        Button(action: {
+                                            UIImpactFeedbackGenerator(style: .light).impactOccurred()
+                                            pathMappingAlertShown = true
+                                        }) {
+                                            HStack {
+                                                Image(systemName: "mappin.circle")
+                                                Text("Button_Add_Path_Mapping_Source")
+                                                    .lineLimit(1)
+                                                    .minimumScaleFactor(0.5)
+                                            }
+                                            .padding(8)
+                                            .frame(maxWidth: .infinity)
+                                            .overlay(
+                                                RoundedRectangle(cornerRadius: 8)
+                                                    .stroke(Color.white.opacity(0.25), lineWidth: 0.5)
+                                            )
+                                        }
+
+                                        Button(action: {
+                                            UIImpactFeedbackGenerator(style: .light).impactOccurred()
+                                            pathMappingRemoveAlertShown = true
+                                        }) {
+                                            HStack {
+                                                Image(systemName: "mappin.slash.circle")
+                                                Text("Button_Remove_Path_Mapping_Source")
+                                                    .lineLimit(1)
+                                                    .minimumScaleFactor(0.5)
+                                            }
+                                            .padding(8)
+                                            .frame(maxWidth: .infinity)
+                                            .overlay(
+                                                RoundedRectangle(cornerRadius: 8)
+                                                    .stroke(Color.white.opacity(0.25), lineWidth: 0.5)
+                                            )
+                                        }
+                                        .padding(.bottom)
+                                    }
                                     Button(action: {
                                         UIImpactFeedbackGenerator(style: .light).impactOccurred()
                                         mobilePasswordChangeAlertShown = true
@@ -79,8 +130,8 @@ struct SettingsView: View {
                                         )
                                     }
                                     .padding(.bottom)
-                                    
-                                    
+
+
                                     Button(action: {
                                         UIImpactFeedbackGenerator(style: .light).impactOccurred()
                                         isSelectingPackageManagers = true
@@ -120,23 +171,25 @@ struct SettingsView: View {
                                                 .stroke(Color.white.opacity(0.25), lineWidth: 0.5)
                                         )
                                     }
-                                    Button(action: {
-                                        UIImpactFeedbackGenerator(style: .light).impactOccurred()
-                                        removeJailbreakAlertShown = true
-                                    }) {
-                                        HStack {
-                                            Image(systemName: "trash")
-                                            Text("Button_Remove_Jailbreak")
-                                                .lineLimit(1)
-                                                .minimumScaleFactor(0.5)
-                                        }
-                                        .padding(.horizontal, 4)
-                                        .padding(8)
-                                        .frame(maxWidth: .infinity)
-                                        .overlay(
-                                            RoundedRectangle(cornerRadius: 8)
-                                                .stroke(Color.white.opacity(0.25), lineWidth: 0.5)
-                                        )
+                                    if !isJailbroken() {
+                                      Button(action: {
+                                          UIImpactFeedbackGenerator(style: .light).impactOccurred()
+                                          removeJailbreakAlertShown = true
+                                      }) {
+                                          HStack {
+                                              Image(systemName: "trash")
+                                              Text("Button_Remove_Jailbreak")
+                                                  .lineLimit(1)
+                                                  .minimumScaleFactor(0.5)
+                                          }
+                                          .padding(.horizontal, 4)
+                                          .padding(8)
+                                          .frame(maxWidth: .infinity)
+                                          .overlay(
+                                              RoundedRectangle(cornerRadius: 8)
+                                                  .stroke(Color.white.opacity(0.25), lineWidth: 0.5)
+                                          )
+                                      }
                                     }
                                     Text(isJailbroken() ? "Hint_Hide_Jailbreak_Jailbroken" : "Hint_Hide_Jailbreak")
                                         .font(.footnote)
@@ -154,7 +207,7 @@ struct SettingsView: View {
                     .tint(.accentColor)
                     .padding(.vertical, 16)
                     .padding(.horizontal, 32)
-                    
+
                     Divider()
                         .background(.white)
                         .padding(.horizontal, 32)
@@ -168,15 +221,33 @@ struct SettingsView: View {
                             .opacity(0.6)
                     }
                     .padding(.top, 2)
-                    
+
                     if easterEgg {
                         Image("fr")
                             .resizable()
                             .aspectRatio(contentMode: .fit)
                             .frame(maxHeight: .infinity)
                     }
-                    
+
                     ZStack {}
+                        .textFieldAlert(isPresented: $pathMappingAlertShown) { () -> TextFieldAlert in
+                            TextFieldAlert(title: NSLocalizedString("Popup_Path_Mapping_Source_Title", comment: ""),
+                                         message: NSLocalizedString("Message_Path_Mapping_Source", comment: ""),
+                                            text: Binding<String?>($pathMappingInput),
+                                        onSubmit: {
+                                bindMount(path: pathMappingInput)
+                            })
+                        }
+                        .textFieldAlert(isPresented: $pathMappingRemoveAlertShown) { () -> TextFieldAlert in
+                            TextFieldAlert(title: NSLocalizedString("Popup_Remove_Path_Mapping_Source_Title",
+                                                                    comment: ""),
+                                         message: NSLocalizedString("Message_Remove_Path_Mapping_Source",
+                                                                    comment: ""),
+                                            text: Binding<String?>($pathMappingRemoveInput),
+                                        onSubmit: {
+                                    bindUnmount(path: pathMappingRemoveInput)
+                            })
+                        }
                         .textFieldAlert(isPresented: $mobilePasswordChangeAlertShown) { () -> TextFieldAlert in
                             TextFieldAlert(title: NSLocalizedString("Popup_Change_Mobile_Password_Title", comment: ""), message: NSLocalizedString("Popup_Change_Mobile_Password_Message", comment: ""), text: Binding<String?>($mobilePasswordInput), onSubmit: {
                                 changeMobilePassword(newPassword: mobilePasswordInput)
@@ -195,10 +266,10 @@ struct SettingsView: View {
                             }
                         }, message: { Text("Alert_Tweak_Injection_Toggled_Body") })
                         .frame(maxHeight: 0)
-                    
+
                 }
                 .foregroundColor(.white)
-                
+
             } else {
                 PackageManagerSelectionView(shown: $isSelectingPackageManagers, reinstall: true) {
                     isSelectingPackageManagers = false
@@ -215,7 +286,7 @@ struct SettingsView: View {
             }
         }
     }
-    
+
     func successRate() -> String {
         if totalJailbreaks == 0 {
             return "-"
@@ -236,7 +307,7 @@ extension View {
         when shouldShow: Bool,
         alignment: Alignment = .leading,
         @ViewBuilder placeholder: () -> Content) -> some View {
-            
+
             ZStack(alignment: alignment) {
                 placeholder().opacity(shouldShow ? 1 : 0)
                 self
